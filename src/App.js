f78eaa1483f0f26ee280cd871141fbef86579974
Salva Roca <salva.roca@icloud.com>
2024-06-20 19:14:52 +0200

    feat: model fetching, response loading and error handling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import {Button} from 'primereact/button';
 import {FileUpload} from 'primereact/fileupload';
 import {Tooltip} from "primereact/tooltip";
 import {Toast} from 'primereact/toast';
-import {Tag} from 'primereact/tag';
-import {useState, useRef} from "react";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
 import {SelectButton} from "primereact/selectbutton";
 import {InputTextarea} from "primereact/inputtextarea";
 import {Menubar} from "primereact/menubar";
 import {ChatMessage} from "./components/ChatMessage";
+import {ProgressSpinner} from "primereact/progressspinner";
 
 export const App = () => {
     const [visible, setVisible] = useState(false);
-    const [totalSize, setTotalSize] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isModelLoaded, setIsModelLoaded] = useState(false);
     const [llm, setLlm] = useState('mistral');
     const [rag, setRag] = useState('langchain');
     const [queryText, setQueryText] = useState('');
-    const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState(() => {
+        const storedMessages = localStorage.getItem('messages');
+        return storedMessages ? JSON.parse(storedMessages) : [];
+    });
     const toast = useRef(null);
     const fileUploadRef = useRef(null);
+
+    useEffect(() => {
+        handleFetchModel();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+            localStorage.setItem('messages', JSON.stringify(messages));
+        }, [messages]
+    )
+
+    useLayoutEffect(() => {
+        document.getElementById('message-container').scrollTo({
+            top: document.getElementById('message-container').scrollHeight,
+            behavior: 'smooth',
+        });
+    }, [messages]);
 
     const llmOptions = [
         {label: 'Mistral', value: 'mistral'},
@@ -47,42 +68,17 @@ export const App = () => {
         }
     ]
 
-    const onSelect = (e) => {
-        let _totalSize = totalSize;
-        let files = e.files;
-
-        Object.keys(files).forEach((key) => {
-            _totalSize += files[key].size || 0;
-        });
-
-        setTotalSize(_totalSize);
-    };
-
-    const onUpload = (e) => {
-        let _totalSize = 0;
-
-        e.files.forEach((file) => {
-            _totalSize += file.size || 0;
-        });
-
-        setTotalSize(_totalSize);
+    const onUpload = () => {
         setVisible(false);
-        toast.current.show({severity: 'info', summary: 'Info', detail: 'Modelo cargado'});
+        handleFetchModel();
     };
 
     const onRemove = (file, callback) => {
-        setTotalSize(totalSize - file.size);
         callback();
     };
 
     const onError = () => {
-        setTotalSize(0);
         toast.current.show({severity: 'error', summary: 'Error', detail: 'Error al cargar el modelo'});
-
-    };
-
-    const onClear = () => {
-        setTotalSize(0);
     };
 
     const headerTemplate = (options) => {
@@ -100,14 +96,12 @@ export const App = () => {
     const itemTemplate = (file, props) => {
         return (
             <div className="flex align-items-center flex-wrap">
-                <div className="flex align-items-center" style={{width: '40%'}}>
+                <div className="flex align-items-center" style={{width: '90%'}}>
                     <img alt={file.name} role="presentation" src={file.objectURL} width={100}/>
                     <span className="flex flex-column text-left ml-3">
                         {file.name}
-                        <small>{new Date().toLocaleDateString()}</small>
                     </span>
                 </div>
-                <Tag value={props.formatSize} severity="warning" className="px-3 py-2"/>
                 <Button type="button" icon="pi pi-times"
                         className="p-button-outlined p-button-rounded p-button-danger ml-auto"
                         onClick={() => onRemove(file, props.onRemove)}/>
@@ -137,16 +131,19 @@ export const App = () => {
         iconOnly: true,
         className: 'custom-choose-btn p-button-rounded p-button-outlined'
     };
+
     const uploadOptions = {
         icon: 'pi pi-fw pi-cloud-upload',
         iconOnly: true,
         className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined'
     };
+
     const cancelOptions = {
         icon: 'pi pi-fw pi-times',
         iconOnly: true,
         className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined'
     };
+
     const footerContent = (
         <div className="card flex justify-content-center">
             <SelectButton value={llm} onChange={(e) => setLlm(e.value)} options={llmOptions}
@@ -165,24 +162,57 @@ export const App = () => {
         }
     };
 
-    const handleSubmit = () => {
-        if (queryText.trim()) {
-            setMessages([...messages, {
-                author: 'Usuario',
-                text: queryText,
-                timestamp: new Date().toLocaleString()
-            }]);
-            setQueryText('');
+    const fetchModel = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:5000/models');
 
-            fetchQueries(queryText)
-                .then(botMessage => {
-                    setMessages(prevMessages => [...prevMessages, botMessage]);
-                })
-                .catch(error => {
-                    console.error('Error en la solicitud Fetch:', error);
+            if (response.ok) {
+                return await response.json();
+            } else if (response.status === 400) {
+                toast.current.show({
+                    severity: 'warn',
+                    summary: 'Advertencia',
+                    detail: 'No se ha cargado ningún modelo'
                 });
+            } else {
+                toast.current.show({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: 'Ocurrió un error al obtener detalles del modelo'
+                });
+            }
+        } catch (error) {
+            throw error;
         }
     };
+
+    const handleFetchModel = () => {
+        fetchModel()
+            .then(modelData => {
+                if (modelData) {
+                    setIsModelLoaded(true);
+                    toast.current.show({
+                        severity: 'info',
+                        summary: 'Info',
+                        detail: 'Modelo cargado: ' + modelData.llm + ' - ' + modelData.rag
+                    });
+                } else {
+                    setIsModelLoaded(false);
+                    setVisible(true);
+                }
+            })
+            .catch(error => {
+                toast.current.show({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: 'Ocurrió un error al obtener detalles del modelo'
+                });
+                console.error('Error al obtener detalles del modelo:', error);
+                setIsModelLoaded(false);
+                setVisible(true);
+            })
+    };
+
 
     const fetchQueries = async (query) => {
         try {
@@ -191,7 +221,10 @@ export const App = () => {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ query: query })
+                body: JSON.stringify({
+                    query: query,
+                    messages: messages
+                })
             });
 
             if (response.ok) {
@@ -205,47 +238,87 @@ export const App = () => {
                 console.error('Error en la respuesta:', response.status, response.statusText);
             }
         } catch (error) {
-            console.error('Error en la solicitud Fetch:', error);
             throw error;
         }
     };
 
+    const handleSubmit = () => {
+        setIsLoading(true);
+
+        if (queryText.trim()) {
+            setMessages(prevMessages => [...prevMessages, {
+                author: 'Usuario',
+                text: queryText,
+                timestamp: new Date().toLocaleString()
+            }]);
+
+            setQueryText('');
+            console.log("Hola Angelina");
+
+            fetchQueries(queryText)
+                .then(botMessage => {
+                    setMessages(prevMessages => [...prevMessages, botMessage]);
+                })
+                .catch(error => {
+                    toast.current.show({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: 'Error al obtener respuesta del modelo'
+                    });
+                    console.error('Error al obtener respuesta del modelo:', error);
+                    setIsModelLoaded(false);
+                    setVisible(true);
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                });
+        } else {
+            setIsLoading(false);
+        }
+    };
+
     return (
-        <>
-            <Menubar model={menuItems} className="surface-ground"/>
+        <div className="h-screen flex flex-column">
+            <Menubar model={menuItems} className="surface-ground top-0 left-0 right-0 w-full gap-2"/>
             <Toast ref={toast}></Toast>
 
-            <div>
+            <div id="message-container" className="flex-grow-1 overflow-auto">
                 {messages.map((message, index) => (
                     <ChatMessage message={message} key={index}/>
                 ))}
             </div>
 
             <div
-                className="surface-ground px-2 py-2 fixed bottom-0 left-0 right-0 flex justify-content-center align-items-center gap-2">
+                className="surface-ground bottom-0 w-full px-2 py-2 flex justify-content-center align-items-center gap-2">
                 <InputTextarea autoResize value={queryText}
                                onChange={(e) => setQueryText(e.target.value)}
                                onKeyDown={handleKeyDown}
                                rows={1}
                                className="border-primary w-full justify-content-end"/>
-                <Button icon="pi pi-send" label="Enviar" onClick={handleSubmit}/>
+                <Button
+                    icon={!isLoading ? "pi pi-send" : null}
+                    label={!isLoading ? "Enviar" : null}
+                    onClick={handleSubmit}
+                    disabled={!isModelLoaded || isLoading || !queryText.trim()}
+                >
+                    {isLoading && <ProgressSpinner style={{width: '20px', height: '20px'}} strokeWidth="8" />}
+                </Button>
             </div>
-
-            <Dialog header="Subir archivos al modelo" style={{width: '50vw'}} visible={visible}
+            <Dialog header="Subir archivos al modelo" style={{width: '50vw'}} visible={visible} draggable={false}
+                    closable={isModelLoaded}
                     onHide={() => setVisible(false)} footer={footerContent}>
-
 
                 <Tooltip target=".custom-choose-btn" content="Explorar" position="bottom"/>
                 <Tooltip target=".custom-upload-btn" content="Cargar" position="bottom"/>
                 <Tooltip target=".custom-cancel-btn" content="Vaciar" position="bottom"/>
 
                 <FileUpload ref={fileUploadRef} name="files" url={`http://127.0.0.1:5000/models?llm=${llm}&rag=${rag}`}
-                            multiple accept="application/pdf" onUpload={onUpload} onSelect={onSelect} onError={onError}
-                            onClear={onClear} headerTemplate={headerTemplate} itemTemplate={itemTemplate}
+                            multiple accept="application/pdf" onUpload={onUpload} onError={onError}
+                            headerTemplate={headerTemplate} itemTemplate={itemTemplate}
                             emptyTemplate={emptyTemplate} chooseOptions={chooseOptions} uploadOptions={uploadOptions}
                             cancelOptions={cancelOptions}/>
             </Dialog>
-        </>
+        </div>
     );
 }
 
